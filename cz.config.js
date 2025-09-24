7283b9815971a34f50cab18ce4213992215c37c0
Lumirelle <shabbyacc@outlook.com>
2025-09-25 00:03:59 +0800

    chore: update cz config & taze config

--- cz.config.js
+++ cz.config.js
@@ -4,7 +4,7 @@ import path from 'node:path'
 import { definePrompt } from 'czg'
 
 /* -------------------------- Config for generation ------------------------- */
-const modulesDir = 'app'
+const modulesDir = '.'
 
 /* ---------------------------- Scopes generation --------------------------- */
 /**
@@ -12,8 +12,11 @@ const modulesDir = 'app'
  * @return {string[]} names of directories under `dir`
  */
 function getModuleNames(dir) {
+  if (!dir || dir === '') {
+    return []
+  }
   return fs.readdirSync(path.resolve(import.meta.dirname, dir)).filter((file) => {
-    return fs.statSync(path.resolve(import.meta.dirname, dir, file)).isDirectory()
+    return fs.statSync(path.resolve(import.meta.dirname, dir, file)).isDirectory() && !file.match(/^\.|node_modules/)
   })
 }
 
@@ -45,13 +48,6 @@ export default definePrompt({
   },
 
   scopes: moduleScopes,
-  scopeOverrides: {
-    chore: [
-      ...moduleScopes,
-      { value: 'deps', name: `${formatName('deps: ', maxLenModuleName)} A dependencies change` },
-      { value: 'tools', name: `${formatName('tools: ', maxLenModuleName)} A tools and utilities change` },
-    ],
-  },
 
   allowBreakingChanges: ['feat', 'fix', 'chore'],
   markBreakingChangeMode: true,
